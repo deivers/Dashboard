2ed28b24faa43cb0ebe606baa1716d96ae104329
Doug Ivers <douglasivers@gmail.com>
2013-12-08 21:24:46 -0500

    Add LogResponse file and supporting code to Sequencer   - tested on server

---
+++ Quizzes/Sequencer/LogResponse.php
@@ -0,0 +1,32 @@
+<?php
+session_cache_limiter('nocache');
+header('Content-type: application/json');
+include( '../../_code/logger.php' );
+
+$workingDir = getcwd(); // get current working directory
+error_log("LogResponse.php  in  " . $workingDir);
+$studentId = $_POST['si'];  //todo: get it from the environment variable REMOTE_USER
+$questionNumber = $_POST['qn'];
+$questionType = $_POST['qt'];
+$questionTextSummary = $_POST['qs'];
+$studentAnswerArray = $_POST['sa'];
+for ($i=0; $i<count($studentAnswerArray); $i++) {
+	$ak[$i] = $i;
+}
+//error_log("/t".$workingDir." ".$studentId." ".$questionNumber." ".$questionType." ".$questionTextSummary." ".$studentAnswerArray);
+$result = logStudentSubmission(
+		$workingDir,
+		$studentId,
+		$questionNumber,
+		$questionType,
+		$questionTextSummary,
+		$studentAnswerArray,
+		$ak
+);
+//todo: if ($result <= 0) notify the user to try again
+if ($result <= 0) {
+	date_default_timezone_set('UTC');
+	error_log("*** Error logging response: ".date("c")."|".$studentId."|".$questionNumber."|".$questionType."|".$questionTextSummary."|".$studentAnswerArray);
+}
+
+?>
--- _code/Sequencer.js
+++ _code/Sequencer.js
@@ -4,7 +4,7 @@ var rejectOption = (typeof rejectWrongAnswers === 'undefined' || rejectWrongAnsw
 var studentList = [];
 var dragList;
 var allList;
-var akList;
+var akList = [];
 $('.draggable').draggable();
 $(function() {
 	$("#sortable1, #sortable2").sortable({
@@ -28,27 +28,71 @@ $(function() {
 	$("#nextButton").click(function(){window.open(nextPageUrl,"_self")})
 		.hide();      // this will be revealed when the quiz is correctly completed
 	$("body").append(showMetaInfo(version));
+});
 
-	function checkAnswers() {
-		var allCorrect = true;
-		for (var i=0; i<allList.length; i++) {
-			if (allList[i] != $(".draggable")[i]) {
-				allCorrect = false;
-				break;
-			}
-		}
-		// give feedback to student
-		if (allCorrect) {
-			$(".draggable").addClass("correct",400);
-			setTimeout(function() {alert("CORRECT!\n\nIf you want to do it again, tap Reset.");}, 600);		// make sure the tabs are moved before the alert shows
-			$("#submitButton").hide();
-			$("#nextButton").show();	// if nextButton exists, show it so student can proceed to next page
-		} else {
-			$(".draggable").removeClass("correct",400);
-			setTimeout(function() {alert("One or more of the items is out of order.  Please try again...");}, 600);		// make sure the tabs are moved before the alert shows
+function checkAnswers() {
+	var allCorrect = true;
+	for (var i=0; i<allList.length; i++) {
+		if (allList[i] != $(".draggable")[i]) {
+			allCorrect = false;
+			break;
 		}
 	}
-	function resetQuiz() {
-		location.reload();
+	/* log answers */
+	if (typeof logResponsesToDashboard === 'undefined')
+		logResponsesToDashboard = false;
+	if (logResponsesToDashboard) {
+		$(".draggable").each(function(index) {
+			studentList.push($(this).html());
+		});
+		$(allList).each(function(index) {
+			akList.push($(this).html());
+		});
+		//console.log(akList);
+		//console.log(studentList);
+		logSubmission(akList,studentList);
 	}
-})
+	// give feedback to student
+	if (allCorrect) {
+		$(".draggable").addClass("correct", 400);
+		setTimeout(function() {alert("CORRECT!\n\nIf you want to do it again, tap Reset.");}, 600);		// make sure the tabs are moved before the alert shows
+		$("#submitButton").hide();
+		$("#nextButton").show();	// if nextButton exists, show it so student can proceed to next page
+	} else {
+		$(".draggable").removeClass("correct", 400);
+		setTimeout(function() {alert("One or more of the items is out of order.  Please try again...");}, 600);		// make sure the tabs are moved before the alert shows
+	}
+}
+
+function resetQuiz() {
+	location.reload();
+}
+
+function logSubmission(answers,sAnswers) {
+	// submit to server
+	if (typeof studentId === 'undefined' || studentId == "")
+		var studentId = prompt("Please enter your student ID","")
+	// todo: get id from the environment variable
+	// todo: verify that we got a unique valid id above, or create one from the ip address?
+	if (typeof quizpageNumber === 'undefined')
+		quizpageNumber = 0;
+	quizpageTextSummary = answers.join(", ");
+	//console.log(quizpageTextSummary);
+	var request = $.ajax({
+		type: 'POST',
+		url: 'LogResponse.php',
+		data: {	si : studentId,		//todo: get student id from env var
+				qn : quizpageNumber,
+				qt : questionType,
+				qs : quizpageTextSummary,
+				sa : sAnswers
+		},
+		dataType: 'json'
+	});
+	request.done(function(msg) {
+		console.log("Submission successful: ");
+	});
+	request.fail(function(jqXHR, textStatus) {
+		console.log("The submission failed: "+textStatus);
+	});
+}
