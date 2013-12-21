893a602499269e5d39d91af7ddee8bb9d9202120
Doug Ivers <douglasivers@gmail.com>
2013-12-21 12:14:31 -0500

    Add parameters to Sequencer1.html, indentation

--- _code/Sequencer.js
+++ _code/Sequencer.js
@@ -30,11 +30,13 @@ $(function() {
 
 function checkAnswers() {
 	var allCorrect = true;
+	var nCorrect = 0;
 	for (var i=0; i<allList.length; i++) {
 		if (allList[i] != $(".draggable")[i]) {
 			allCorrect = false;
 			break;
 		}
+		nCorrect++;
 	}
 	/* log answers */
 	if (typeof logResponsesToDashboard === 'undefined')
@@ -46,17 +48,23 @@ function checkAnswers() {
 		});
 		var saList = convertToIndexes($(".draggable"),allList);
 		var akList = arrayFactory(allList.length);
-		logSubmission(quizpageNumber,questionType,qTextSummary,aDetailedList,saList,akList);
+		var logSuccess = logSubmission(quizpageNumber,questionType,qTextSummary,aDetailedList,saList,akList);
+		if (logSuccess == false) {
+			alert("You must provide a valid student ID in order to see what you got right/wrong.");
+			return;
+		}
 	}
 	// give feedback to student
 	if (allCorrect) {
 		$(".draggable").addClass("correct", 400);
-		setTimeout(function() {alert("CORRECT!\n\nIf you want to do it again, tap Reset.");}, 600);		// make sure the tabs are moved before the alert shows
+		setTimeout(function() {alert("CORRECT!\n\nIf you want to do it again, tap Reset.");}, 600);		// make sure the animation is finished before the alert shows
 		$("#submitButton").hide();
 		$("#nextButton").show();	// if nextButton exists, show it so student can proceed to next page
 	} else {
-		$(".draggable").removeClass("correct", 400);
-		setTimeout(function() {alert("One or more of the items is out of order.  Please try again...");}, 600);		// make sure the tabs are moved before the alert shows
+		if (showWrongAnswers)
+			addClassToDraggables(0,nCorrect,"correct",400);
+		else
+			setTimeout(function() {alert("One or more of the items is out of order.  Please try again...");}, 600);		// make sure the animation is finished before the alert shows
 	}
 }
 
@@ -64,3 +72,11 @@ function resetQuiz() {
 	location.reload();
 }
 
+function addClassToDraggables(start,stop,className,duration) {
+	$(".draggable").each(function(index){
+		if (start <= index && index < stop)
+			$(this).addClass(className,duration);
+		else
+			$(this).removeClass(className,duration);
+	});
+}
