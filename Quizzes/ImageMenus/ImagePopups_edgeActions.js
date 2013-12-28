17d2e356f78fce7c226b08c08cecec071ef1af95
Doug Ivers <douglasivers@gmail.com>
2013-12-28 17:14:47 -0500

    Refactor ImageLabeler to move code into external js file

--- Quizzes/ImageMenus/ImagePopups_edgeActions.js
+++ Quizzes/ImageMenus/ImagePopups_edgeActions.js
@@ -43,79 +43,18 @@ var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonl
          
          console.log("composition ready");
          
-         
-         sym.checkStudentAnswers = function() {
-         	console.log("checking answers");
-         	var answerWidgets = sym.$(".menu");
-         	var wrong = [];
-         	var k = 0;
-         	var answerTexts = [];
-         	var isComplete = true;
-         	// which index did the student pick?
-         	for (var j=0; j<answerWidgets.length; j++) {
-         		answerTexts.push($(answerWidgets[j]).val());
-         		thisText = $(answerWidgets[j]).val();
-         		if (thisText != texts[j]) {
-         			// this one is incorrect
-         			wrong[k++] = answerWidgets[j];
-         			if (thisText == "?" || thisText == "-" || thisText == "")
-         				isComplete = false;
-         		}
-         		//var index = $.inArray($(answerWidgets[j]).val(), textForMenus); // is there an easier way to get the selected index?
-         		//console.log(index);
-         	}
-         	if (isComplete) {
-         		// log answers //
-         		if (typeof logResponsesToDashboard === 'undefined')
-         			logResponsesToDashboard = false;
-         		if (logResponsesToDashboard) {
-         			var saArray = convertToIndexes(answerTexts,texts);
-         			var akArray = arrayFactory(answerTexts.length,1,0);
-         			//console.log(texts); console.log(saArray); console.log(akArray);
-         			var logSuccess = logSubmission(quizpageNumber,questionType,qTextSummary,texts,saArray,akArray);
-         			if (logSuccess == false) {
-         				alert("You must provide a valid student ID for answers to be checked.");
-         				return;
-         			}
-         		}
-         		// scoring //
-         		// clear any previous marks
-         		$(answerWidgets).parent().removeClass("incorrect");
-         		// mark wrong answers
-         		if (showWrongAnswers)
-         			$(wrong).parent().removeClass("correct").addClass("incorrect");
-         		if (wrong.length > 0)
-         			alert("Not all are correct.  Please try again.");
-         		else {
-         			$(answerWidgets).parent().addClass("correct");
-         			alert("All correct!");
-         			sym.getComposition().getSymbols("SubmitAnswersButton")[0].getSymbolElement().css({"opacity":0,"left":-1000});
-         			if (typeof nextPageUrl !== 'undefined' && nextPageUrl != "")
-         				sym.getComposition().getSymbols("NextPageButton")[0].getSymbolElement().css({"opacity":1});
-         		}
-         	} else
-         		alert("You must complete the quiz before answers will be checked.");
-         }
-         
-         sym.goNextPage = function() {
-         	console.log(">>> "+nextPageUrl);
-         	if (typeof nextPageUrl !== 'undefined' && nextPageUrl != "")
-         		window.open(nextPageUrl, "_self");
-         }
-         
 
       });
       //Edge binding end
 
       Symbol.bindElementAction(compId, symbolName, "${_SubmitAnswersButton}", "click", function(sym, e) {
-         sym.getComposition().getStage().checkStudentAnswers();
+         checkStudentAnswers();
 
       });
       //Edge binding end
 
       Symbol.bindElementAction(compId, symbolName, "${_NextPageButton}", "click", function(sym, e) {
-         sym.getComposition().getStage().goNextPage();
-         
+         goNextPage();
 
       });
       //Edge binding end
--- _code/ImageLabeler.js
+++ _code/ImageLabeler.js
@@ -9,7 +9,6 @@ for (var i=0; i<textFields.length; i++) {
 // randomize the menu options
 var textForMenus = texts.slice(0);	// duplicate the array
 shuffleArray(textForMenus);
-console.log("*****");
 var optionString = "<option>?</option>";
 for (var i=0; i<textFields.length; i++) {
 	optionString += "<option>"+textForMenus[i]+"</option>";
@@ -29,3 +28,62 @@ for (var i=0; i<textFields.length; i++) {
 	}
 }
 $(".textSource").css({"opacity": 1});
+
+checkStudentAnswers = function() {
+	console.log("checking answers");
+	var answerWidgets = $(".menu");
+	var wrong = [];
+	var k = 0;
+	var answerTexts = [];
+	var isComplete = true;
+	// which index did the student pick?
+	for (var j=0; j<answerWidgets.length; j++) {
+		answerTexts.push($(answerWidgets[j]).val());
+		thisText = $(answerWidgets[j]).val();
+		if (thisText != texts[j]) {
+			// this one is incorrect
+			wrong[k++] = answerWidgets[j];
+			if (thisText == "?" || thisText == "-" || thisText == "")
+				isComplete = false;
+		}
+		//var index = $.inArray($(answerWidgets[j]).val(), textForMenus); // is there an easier way to get the selected index?
+		//console.log(index);
+	}
+	if (isComplete) {
+		// log answers //
+		if (typeof logResponsesToDashboard === 'undefined')
+			logResponsesToDashboard = false;
+		if (logResponsesToDashboard) {
+			var saArray = convertToIndexes(answerTexts,texts);
+			var akArray = arrayFactory(answerTexts.length,1,0);
+			//console.log(texts); console.log(saArray); console.log(akArray);
+			var logSuccess = logSubmission(quizpageNumber,questionType,qTextSummary,texts,saArray,akArray);
+			if (logSuccess == false) {
+				alert("You must provide a valid student ID for answers to be checked.");
+				return;
+			}
+		}
+		// scoring //
+		// clear any previous marks
+		$(answerWidgets).parent().removeClass("incorrect");
+		// mark wrong answers
+		if (showWrongAnswers)
+			$(wrong).parent().removeClass("correct").addClass("incorrect");
+		if (wrong.length > 0)
+			alert("Not all are correct.  Please try again.");
+		else {
+			$(answerWidgets).parent().addClass("correct");
+			alert("All correct!");
+			/////$(".SubmitAnswersButton")[0].css({"opacity":0,"left":-1000});
+			//if (typeof nextPageUrl !== 'undefined' && nextPageUrl != "")
+			//	$("NextPageButton")[0].getSymbolElement().css({"opacity":1});
+		}
+	} else
+		alert("You must complete the quiz before answers will be checked.");
+}
+
+goNextPage = function() {
+	console.log(">>> "+nextPageUrl);
+	if (typeof nextPageUrl !== 'undefined' && nextPageUrl != "")
+		window.open(nextPageUrl, "_self");
+}
