b7e16f80a296020e26cc2444250acdfc42f205d5
Doug Ivers <douglasivers@gmail.com>
2014-01-28 13:37:20 -0500

    Audio Matchup

---
+++ Quizzes/MatchupStackedAudio/LogResponse.php
@@ -0,0 +1,32 @@
+<?php
+session_cache_limiter('nocache');
+header('Content-type: application/json');
+include( '../../_code/logger.php' );
+
+$workingDir = getcwd(); // get current working directory
+error_log("LogResponse.php  in  " . $workingDir);
+$studentId = $_POST['si'];  //todo: get it from the environment variable REMOTE_USER
+$qNumber = $_POST['qn'];
+$qType = $_POST['qt'];
+$qTextSummary = $_POST['qs'];
+$answerDetailArray = $_POST['ad'];
+$studentAnswerArray = $_POST['sa'];
+$answerArray = $_POST['ak'];
+//error_log("/t".$workingDir." ".$studentId." ".$qNumber." ".$qType." ".$qTextSummary." ".$answerDetailArray." ".$studentAnswerArray." ".$answerArray);
+$result = logStudentSubmission(
+		$workingDir,
+		$studentId,
+		$qNumber,
+		$qType,
+		$qTextSummary,
+		$answerDetailArray,
+		$studentAnswerArray,
+		$answerArray
+);
+//todo: if ($result <= 0) notify the user to try again
+if ($result <= 0) {
+	date_default_timezone_set('UTC');
+	error_log("*** Error logging response: ".date("c")."|".$studentId."|".$qNumber."|".$qType."|".$qTextSummary."|".$studentAnswerArray);
+}
+
+?>
