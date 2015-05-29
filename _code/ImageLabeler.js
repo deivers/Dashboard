bf60f77ae4b9fc45a2bdc1fd7ee162e2820c1642
Doug Ivers <douglasivers@gmail.com>
2015-05-28 21:58:05 -0400

    Set up 4 of the config params as input fields above the stage.  The other configs may ultimately not be needed...

--- _code/ImageLabeler.js
+++ _code/ImageLabeler.js
@@ -2,22 +2,29 @@
 var texts = [];
 var textFields;
 var questionType;
+//
+var answerTypeIsMenus;
+var showWrongAnswers;
+var logResponsesToDashboard;
+var nextPageUrl;
 // globals defined elsewhere but needed herein
-//	imageLabelerType
-//	logResponsesToDashboard
 //	quizpageNumber
 //	qTextSummary
-//	showWrongAnswers
-//	nextPageUrl
 // additional globals needed if ShortAnswer type
 //	minNumChars
 
 function init() {
+	// read the configuration parameters
+	answerTypeIsMenus = ($("#Stage_config-answerTypeIsMenus").html().substring(0,1) == "t");
+	console.log(answerTypeIsMenus +"*");
+	showWrongAnswers = ($("#Stage_config-showWrongAnswers").html().substring(0,1) == "t");
+	logResponsesToDashboard = ($("#Stage_config-logResponsesToDashboard").html().substring(0,1) == "t");
+	nextPageUrl = $("#Stage_config-nextPageUrl").html().specialTrim();
 	// collect all possible answers
 	textFields = $(".textSource");
 	// console.log("number of fields: "+textFields.length);
 	buildTextsArray();
-	if (typeof imageLabelerType !== 'undefined' && imageLabelerType == "menus") {
+	if (answerTypeIsMenus) {
 		questionType = "Image Labeler with Popup Menus";
 		buildMenus();
 	} else {
@@ -133,7 +140,7 @@ checkAnswers = function() {
 	var wrong = [];
 	var isComplete = true;
 	var answerTexts = [];
-	var answerWidgets = (typeof imageLabelerType !== 'undefined' && imageLabelerType == "menus") ? $(".menu") : $(".textBox");
+	var answerWidgets = (answerTypeIsMenus) ? $(".menu") : $(".textBox");
 	// collect student answers
 	var k = 0;
 	for (var j=0; j<answerWidgets.length; j++) {
@@ -182,4 +189,12 @@ checkAnswers = function() {
 		}
 	} else
 		alert("You must complete the quiz before answers will be checked.");
+}
+
+String.prototype.specialTrim = function() {
+	var cutHere = this.indexOf("<");
+	if (cutHere > 0)
+		return this.substring(0,cutHere);
+	else
+		return this.substring(0);
 }
